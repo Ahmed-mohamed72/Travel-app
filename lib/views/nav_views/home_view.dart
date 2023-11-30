74e529bbbedf618d9f3217e0ad7a3c6d032454bc
Ahmed-mohamed72 <80651131@sci.s-mu.edu.eg>
2023-11-30 15:43:54 +0200

    add icon on app bar and app large text

--- lib/views/nav_views/home_view.dart
+++ lib/views/nav_views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:travel_app1/widgets/app_large_text.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -10,6 +11,33 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: const EdgeInsets.only(left: 20, right: 20, top: 70),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Icon(
+                Icons.menu,
+                size: 30,
+                color: Colors.black54,
+              ),
+              Container(
+                height: 40,
+                width: 40,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 40),
+          const AppLargeText(text: "Discover"),
+        ],
+      ),
+    );
   }
 }
